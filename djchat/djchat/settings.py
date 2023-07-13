7c6eb4bcaacf72a37024cfd193bb6fb2353206ea
isaac <isaac@localhost.localdomain>
2023-07-13 14:29:00 +0300

    list api endpoint decorator

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -134,6 +134,6 @@ SPECTACULAR_SETTINGS = {
     "TITLE": "Your Project API",
     "DESCRIPTION": "Your project description",
     "VERSION": "1.0.0",
-    "SERVE_INCLUDE_SCHEMA": False,
+    "SERVE_INCLUDE_SCHEMA": True,
     # OTHER SETTINGS
 }
--- djchat/server/views.py
+++ djchat/server/views.py
@@ -4,12 +4,46 @@ from .serializer import ServerSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from django.db.models import Count
+from .schema import server_list_docs
 
 
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
+    @server_list_docs
     def list(self, request):
+        """
+        Retrieves a list of servers based on the provided filters.
+
+        This method allows filtering servers based on different criteria such as category,
+        quantity, user, server ID, and the option to include the number of members in each server.
+
+        Parameters:
+            - request (HttpRequest): The HTTP request object.
+
+        Returns:
+            - Response: An HTTP response containing a list of serialized servers.
+
+        Raises:
+            - AuthenticationFailed: If the user is not authenticated and tries to filter by user or server ID.
+            - ValidationError: If the server ID provided in the 'by_serverid' parameter is not found.
+
+        Examples:
+            - GET /servers/?category=network&qty=10&by_user=true&with_num_members=true
+                - Retrieves a list of up to 10 servers in the 'network' category that belong to the authenticated user,
+                  including the number of members in each server.
+
+            - GET /servers/?by_serverid=12345
+                - Retrieves the server with the ID '12345' if it exists.
+
+        Note:
+            - The 'category' parameter filters servers by their category name.
+            - The 'qty' parameter limits the number of servers returned.
+            - The 'by_user' parameter filters servers by the authenticated user.
+            - The 'with_num_members' parameter includes the count of members in each server.
+            - The 'by_serverid' parameter filters servers by their specific server ID.
+              If no server is found with the provided ID, a ValidationError is raised.
+        """
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
