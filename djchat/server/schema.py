675e630e21344b2afc61b7351a7a7bbd0aa5cc57
isaac <isaac@localhost.localdomain>
2023-07-13 14:36:50 +0300

    queryset api parameters description

--- djchat/server/schema.py
+++ djchat/server/schema.py
@@ -11,6 +11,30 @@ server_list_docs = extend_schema(
             type=OpenApiTypes.STR,
             location=OpenApiParameter.QUERY,
             description="Category of Servers to retrieve",
-        )
+        ),
+        OpenApiParameter(
+            name="qty",
+            type=OpenApiTypes.INT,
+            location=OpenApiParameter.QUERY,
+            description="Number of servers to retrieve",
+        ),
+        OpenApiParameter(
+            name="by_user",
+            type=OpenApiTypes.BOOL,
+            location=OpenApiParameter.QUERY,
+            description="Filter servers by the current authenticated user (True/False)",
+        ),
+        OpenApiParameter(
+            name="with_num_members",
+            type=OpenApiTypes.BOOL,
+            location=OpenApiParameter.QUERY,
+            description="Include the number of members for each server in the response (True/False)",
+        ),
+        OpenApiParameter(
+            name="by_server_id",
+            type=OpenApiTypes.INT,
+            location=OpenApiParameter.QUERY,
+            description="Filter servers by server id",
+        ),
     ],
 )
