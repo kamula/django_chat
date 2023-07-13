f35f187a1d9952e9563cdaf7201a44a787be5315
isaac <isaac@localhost.localdomain>
2023-07-13 16:08:54 +0300

    category icon upload path

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+
+
+def category_icon_file_path(instance, filename):
+    return f"category/{instance.id}/category_icon/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(upload_to=category_icon_file_path, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
