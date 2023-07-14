2d817dbb8496d0ae7b0b3e3c2b364c7832c38369
isaac <isaac@localhost.localdomain>
2023-07-14 15:08:29 +0300

    update and delete server if item is deleted

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -4,6 +4,14 @@ from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 
 
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icons/{filename}"
+
+
+def server_banner_upload_path(instance, filename):
+    return f"server/{instance.id}/server_banners/{filename}"
+
+
 def category_icon_file_path(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
 
@@ -48,11 +56,28 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
-        
+    banner = models.ImageField(upload_to=server_banner_upload_path, null=True, blank=True)
+    icon = models.ImageField(upload_to=server_icon_upload_path, null=True, blank=True)
 
+    # def save(self, *args, **kwargs):
+    #     self.name = self.name.lower()
+    #     super(Channel, self).save(*args, **kwargs)
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()
-        super(Channel, self).save(*args, **kwargs)
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Server")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == 'banner':
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
